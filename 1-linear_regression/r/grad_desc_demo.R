37987a8a3fe3e891d5f86657ec43888b25607d4c
mgsosna <matt.g.sosna@gmail.com>
2020-06-15 17:26:51 -0400

    Reorganize repo

--- 1-linear_regression/r/grad_desc_demo.R
+++ 1-linear_regression/r/grad_desc_demo.R
@@ -1,22 +1,22 @@
 ######################################################################################################
 # Multivariate linear regression via gradient descent: demo
-# Author: Matt Grobis
+# Author: Matt Sosna
 #
 ######################################################################################################
 # Load functions
 setwd("")   # Set path to function here
-source('grad_desc_lm.R')   
+source('grad_desc_lm.R')
 
 #-----------------------------------------------------------------------------------------------------
 # 1. Relationship between hours studied and exam score
 
    # Generate data
    dat <- gen_data(n_obs = 50, noise = 3, n_features = 1)
-   
+
    # Create color spectrum
    library(RColorBrewer)
    colors <- colorRampPalette(c("red", "orange", "green", "forestgreen"))(n = round(max(dat$y))+1)
-   
+
    # Plot relationship
    plot(dat$X/10 + 0.2, dat$y + 50, pch = 19, col = colors[round(dat$y)], xlab = "Hours studied",
         ylab = "Exam score", font.lab = 2, font.axis = 2, las = 1, cex = 2, cex.main = 1.5,
@@ -25,34 +25,34 @@ source('grad_desc_lm.R')
 
 #---------------------------------------------------------------------------------------------------
 # 2. Various regressions with different MSE
-   
+
    # Three regressions that differ in their MSE
    attempts <- as.matrix(data.frame('beta_0' = c(100, 75, 49.630775),
                                     'beta_1' = c(-5, 0, 9.653769)))
-   
+
    # Plot it
    par(mfrow = c(1,3), oma = c(2, 2, 3, 0))
    for(i in 1:nrow(attempts)){
-      
+
       # Calculate MSE
       model_mse <- mean((gen_preds(attempts[i, ], dat$X/10 + 0.2) - (dat$y + 50))^2)
-      
+
       # Original plot
       plot(dat$X/10 + 0.2, dat$y + 50, pch = 19, col = colors[round(dat$y)], xlab = NA,
            ylab = NA, font.lab = 2, font.axis = 2, las = 1, cex = 2, cex.main = 2,
            main = "MSE:\n", cex.lab = 1.3, cex.axis = 1.2)
       points(dat$X/10 + 0.2, dat$y+50, cex = 2)
       title(paste0("\n", round(model_mse, 3)), col.main = "gray30", cex.main = 1.8)
-      
+
       # Regression
-      lines(seq(0, 6, length = 1000), gen_preds(attempts[i, ], seq(0, 6, length = 1000)), 
+      lines(seq(0, 6, length = 1000), gen_preds(attempts[i, ], seq(0, 6, length = 1000)),
             lwd = 3, lty = 3, col = "gray20")
-      
+
       # Labels for overall plot
       mtext(outer = T, side = 1, font = 2, cex = 1.4, "Hours studied")
       mtext(outer = T, side = 2, font = 2, cex = 1.4, "Exam score")
       mtext(outer = T, side = 3, font = 2, cex = 1.7, "Hours studied versus exam score", padj = -0.4)
-      
+
    }
 
 #---------------------------------------------------------------------------------------------------
@@ -64,55 +64,55 @@ source('grad_desc_lm.R')
    # Generate model
    fit_1dim <- gd_lm(dat$X, dat$y, stop_thresh = 1e-3, full = T)
 
-#---------------------------------------------------------------------------   
+#---------------------------------------------------------------------------
 # 3b. Number of iterations to convergence at the 1e-3 level vs. dimensions
-   
+
    # Set parameters
    n_dimensions <- 1:6
    num_runs <- 1000
    num_iter <- 10000    # High value so model runs not artificially constrained
-   
+
    # Create empty matrix
    iter_to_converge <- matrix(NA, ncol = length(n_dimensions), nrow = num_runs_iter)
-   
+
    # Iterate through dimensions
    for(i in 1:length(n_dimensions)){
-      
+
       # Generate n-dimensional data
       dat <- gen_data(n_obs = 50, n_features = n_dimensions[i])
-      
+
       # Generate model
       fit <- gd_lm(dat$X, dat$y, stop_thresh = 1e-3, full = T, n_iter = num_iter_iter,
                    n_runs = num_runs_iter)
-      
+
       # Save the number of iterations it took to get to 1e-3 stopping point
       iter_to_converge[, i] <- fit$run_outcomes[,3]
-      
+
       print(i)
    }
-   
+
 #-----------------------------------------------------------------------------------------
 # Set colors for plots
 # - MSE plot: distinct colors
 # - Iterations plot: color by mean number of iterations for that dimension
-   
+
    library(scales)
    library(RColorBrewer)
-   
+
    # MSE plot
    mse_colors <- c("black", "orange", "firebrick2", "forestgreen", "deepskyblue4")
-   
+
    # Boxplots
    mean_iter <- apply(iter_to_converge, 2, mean)
 
    colors <- colorRampPalette(c("deepskyblue4", "orange", "firebrick2"))(n = max(mean_iter))
    boxplot_cols <- colors[mean_iter]
-   
+
 #-------------------------------------------------------------------------------------------
 # Plots
-   
+
    par(mfrow = c(1,2), font.axis = 2, font.lab = 2, cex.main = 1.5, cex.lab = 1.5, cex.axis = 1.1)
-   
+
    # MSE plot
    plot(NA, log = 'y', xlab = "Iteration", ylab = "Mean Squared Error",
         xlim = c(0, max(fit$run_outcomes[,3])),
@@ -121,17 +121,17 @@ source('grad_desc_lm.R')
    title("\nN dimensions = 1", col.main = "gray40")
    par(font = 4)
    legend("topright", bty = 'n', col = mse_colors, pch = 19, legend = paste0("Run", 1:5))
-   
+
    for(i in 1:ncol(fit$MSE_paths)){
       points(fit$MSE_paths[,i], col = alpha(mse_colors[i], 0.8), cex = 1.3, pch = 19)
    }
-   
+
    # Number of iterations
    boxplot(itc, main = "Number of iterations until convergence\n",
            outline = F, font.lab = 2, xlab = "N dimensions in input data",
            ylab = "N iterations", border = boxplot_cols, lwd = 2)
-   title("\nThreshold = 1e-3", col.main = "gray40")  
-   
+   title("\nThreshold = 1e-3", col.main = "gray40")
+
 ################################################################################################
 # 4. Number of runs versus number of iterations heat map
 
@@ -139,103 +139,101 @@ source('grad_desc_lm.R')
    range_n_dim_X <- 1:6
    range_n_runs <- 1:5
    range_n_iter <- c(50, 100, 250, 500, 1000, 5000)
-   
-   
+
+
    # Create empty arrayss
    MSE_diff_raw <- MSE_diff_prop <-  array(NA, dim = c(length(range_n_dim_X),
                                                        length(range_n_runs),
                                                        length(range_n_iter)))
-   
+
    # Name arrays
-   dimnames(MSE_diff_raw) <- dimnames(MSE_diff_prop) <- list(range_n_dim_X, 
-                                                             range_n_runs, 
+   dimnames(MSE_diff_raw) <- dimnames(MSE_diff_prop) <- list(range_n_dim_X,
+                                                             range_n_runs,
                                                              range_n_iter)
-   
+
    #-------------------------------------------------------------------
    # The data at smaller dimensions can be particularly noisy, so let's run the analysis
    # many times and then take the mean.
-   
+
    num_repeats <- 1000
-   
+
    # Iterate through dimensionality of data
    for(i in 1:length(range_n_dim_X)){
-      
+
       # Generate the data
       dat <- gen_data(n_obs = 50, n_features = range_n_dim_X[i])
-      
+
       # Iterate through number of runs
       for(j in 1:length(range_n_runs)){
-         
+
          # Iterate through number of iterations
          for(k in 1:length(range_n_iter)){
-            
+
             # To deal with noise, run each parameter combination 5 times, then take the average
             rep_MSEs_raw <- rep_MSEs_prop <- c()
-            
+
             for(replicate in 1:num_repeats){
-               
+
                # Print progress
-               cat("Processing - N dimensions:", range_n_dim_X[i], 
+               cat("Processing - N dimensions:", range_n_dim_X[i],
                    "| N runs:", range_n_runs[j],
                    "| N iterations:", range_n_iter[k],
                    "| replicate:", replicate,
                    "\n")
-               
+
                # Run the model
                mod <- custom_lm(dat$X, dat$y, n_runs = range_n_runs[j], n_iter = range_n_iter[k])
-               
+
                # Save this replicate's difference in MSE
                # - [,1] and [1, 2] at end is necessary b/c data frame
                rep_MSEs_raw[replicate] <- (mod$MSE[1] - mod$MSE[2])[,1]
                rep_MSEs_prop[replicate] <- rep_MSEs_raw[replicate] / mod$MSE[1,2]
             }
-            
+
             # Save the mean replicate MSE for this parameter combination
             MSE_diff_raw[i, j, k] <- mean(rep_MSEs_raw)
             MSE_diff_prop[i, j, k] <- mean(rep_MSEs_prop)
-            
+
          }
       }
    }
-   
+
    #--------------------------------------------------------------------------------------
    # Let's visualize some stuff
-   
+
    library(lattice)
    library(RColorBrewer)
    library(grid)
    library(gridExtra)
-   
+
    # Set up color gradient
-   
+
    # Raw breaks
    raw_breaks <- seq(min(MSE_diff_raw), max(MSE_diff_raw)+1, by = 0.01)
    raw_cols <- colorRampPalette(c("white", "orange", "firebrick2", "brown"))(length(raw_breaks)-1)
-   
+
    # Proportion breaks
    prop_breaks <- seq(0,35, by = 0.01)
-   prop_cols <- colorRampPalette(c("white", "orange", "firebrick2", "brown"), 
+   prop_cols <- colorRampPalette(c("white", "orange", "firebrick2", "brown"),
                                  bias = 3.5)(length(prop_breaks)-1)
-   
+
    # Save plots as variables
    raw_list <- prop_list <- list()
-   
+
    for(i in 1:length(range_n_dim_X)){
-      
+
       prop_list[[i]] <- levelplot(MSE_diff_prop[i, , ], at = prop_breaks, col.regions = prop_cols,
                                   xlab = " ", ylab = " ", main = paste0("N dimensions = ", i),
                                   scales = list(font=2))
    }
-   
+
    #-------------------------------------------------------------------------------
-   grid.arrange(prop_list[[1]], prop_list[[2]], prop_list[[3]], prop_list[[4]], 
+   grid.arrange(prop_list[[1]], prop_list[[2]], prop_list[[3]], prop_list[[4]],
                 prop_list[[5]], prop_list[[6]], ncol = 3,
-                top = textGrob("MSE difference (proportion)", 
+                top = textGrob("MSE difference (proportion)",
                                gp = gpar(fontsize = 28, font = 2)),
-                
+
                 bottom = textGrob("Number of runs",
                                   gp = gpar(fontsize = 20, font = 2), y = 1),
                 left = textGrob("Number of iterations",
                                 gp = gpar(fontsize = 20, font = 2), rot = 90, x = 1))
-
- 
--- 1-linear_regression/r/grad_desc_lm.R
+++ 1-linear_regression/r/grad_desc_lm.R
@@ -1,6 +1,6 @@
 ######################################################################################################
 # Multivariate linear regression via gradient descent
-# Author: Matt Grobis
+# Author: Matt Sosna
 #
 # - Purpose:
 #     o Demonstrate iterative approach to calculating linear regression coefficients
@@ -17,18 +17,18 @@ try(library('scatterplot3d'),
 
 # 1. Generate structured data
 # - Makes it much easier to test ideas on data with different dimensions
-# - Creates X and y data with a positive relationship. Strength of relationship can 
+# - Creates X and y data with a positive relationship. Strength of relationship can
 #   be modified with 'noise' parameter
 
 gen_data <- function(n_obs, n_features, noise = 3){
-   
+
    # Generate data
    X <- matrix(1:n_obs + rnorm(n_obs*n_features, sd = noise), ncol = n_features)
    y <- 1:n_obs + rnorm(n_obs, sd = noise)
-   
+
    # Label X
    colnames(X) <- paste0("feature_", 1:n_features)
-   
+
    return(list("X" = X, "y" = y))
 }
 
@@ -40,27 +40,27 @@ gen_data <- function(n_obs, n_features, noise = 3){
 # - Requires X to be more than 1 dimension
 
 analytical_reg <- function(X, y){
-   
+
    # Prepare X
    X <- as.matrix(X)
-   
+
    # If X is 1-dimensional, stop
    if(ncol(X) == 1){
       stop("Error: ncol(X) must be greater than 1. Use 'lm' instead.\n")
    }
-   
+
    # Create column of 1's
    X <- cbind(1, X)
-   
+
    # Perform matrix multiplications to get coefficients
    lm_coeffs <- solve(t(X) %*% X) %*% t(X) %*% y
-   
+
    rownames(lm_coeffs) <- c("intercept", paste0("beta_", 1:(nrow(lm_coeffs)-1)))
-   
+
    # Generate predictions to get MSE
    lm_preds <- lm_coeffs[1] + rowSums(sweep(X[, -1], MARGIN = 2, lm_coeffs[2:length(lm_coeffs)], "*"))
    lm_MSE <- mean((lm_preds - y)^2)
-   
+
    # Return list
    our_list <- list("betas" = t(lm_coeffs), "MSE" = lm_MSE)
    return(our_list)
@@ -72,16 +72,16 @@ analytical_reg <- function(X, y){
 # - Function takes in coefficients and input data and outputs model predictions
 
 gen_preds <- function(input_coeffs, input_data){
-   
+
    # Make it a matrix
    input_data <- as.matrix(input_data)
-   
+
    # Check dimensionality of input data
    if(ncol(input_data) == 1){
-          
+
           # Can't use sweep if 1-dimensional X data
           return(input_coeffs[1] + input_coeffs[2] * input_data)
-      
+
    } else {
           # Use sweep if higher-dimension data
           return(input_coeffs[1] + rowSums(sweep(input_data, MARGIN = 2, input_coeffs[-1], "*")))}
@@ -91,193 +91,193 @@ gen_preds <- function(input_coeffs, input_data){
 # 4. Perform multivariate linear regression via gradient descent
 gd_lm <- function(X, y, alpha = 1e-4, n_iter = 1000, stop_thresh = 1e-5, n_runs = 5,
                   figure = F, full = F){
-   
+
    # Note: user has to specify what the features (X) and what the output (y) are
    # - X and y can be in matrix or data.frame format
-   
+
    # Convert X to matrix
    X <- as.matrix(X)
-   
+
    run_vals <- matrix(NA, ncol = ncol(X) + 3, nrow = n_runs)
    colnames(run_vals) <- c("MSE", "num_iter", paste0("beta_", seq(0, ncol(X))))
-   
+
    # Create matrix to hold all MSE and coefficient paths
    if(full == T){
-      
+
       MSE_matrix <- matrix(NA, nrow = n_iter, ncol = n_runs)
       colnames(MSE_matrix) <- paste0("run", 1:n_runs)
       rownames(MSE_matrix) <- paste0("iter", 1:n_iter)
-      
+
       coef_array <- array(NA, dim = c(n_iter, n_runs, ncol(X) + 1))
-      dimnames(coef_array) <- list(paste0("iter", 1:n_iter), 
+      dimnames(coef_array) <- list(paste0("iter", 1:n_iter),
                                    paste0("run", 1:n_runs),
                                    paste0("beta_", seq(0, ncol(X))))
-      
+
    }
-   
+
    # Go through the runs
    for(run in 1:n_runs){
-      
+
       # Create empty matrix of betas matching number of features
       coeffs <- matrix(NA, ncol = ncol(X) + 1, nrow = n_iter)
       colnames(coeffs) <- paste0('beta_', 0:(ncol(X)))
-      
+
       # Initialize the betas at random values close to zero
       coeffs[1, ] <- rnorm(ncol(coeffs), mean = 0, sd = 3)
-      
+
       #-------------------------------------------------------------------------------------------
-      # Create empty variables to store mean squared error and number of iterations at convergence 
-      
+      # Create empty variables to store mean squared error and number of iterations at convergence
+
       MSE_run <- c()
       final_iter <- c()
-      
+
       ##################################################################################
       # Iterate
       for(iter in 2:n_iter){
-         
+
          # Generate predictions
          predictions <- coeffs[iter-1, 1] +                          # Intercept
             rowSums(sweep(X, MARGIN = 2, coeffs[iter-1, -1], "*"))   # Slopes for all features
-         
+
          #-----------------------------------------------------------------------------
          # Update betas based on residuals
-         
+
          # 1. Intercept
          coeffs[iter, 1] <- coeffs[iter-1, 1] - alpha * mean(predictions - y)
-         
+
          # 2. Slopes
          coeffs[iter, -1] <- coeffs[iter-1, -1] - alpha * apply((predictions - y) * X, 2, mean)
-         
+
          #-------------------------------------------------------------------------------
          # Record MSE
          MSE_run[iter] <- mean((predictions - y)^2)
-         
+
          # Stop iterating if MSE not improving substantially
          final_iter <- iter
-         
+
          if(iter > 3 & ((MSE_run[iter - 1] - MSE_run[iter]) < stop_thresh)){break}
-         
+
       }
-      
+
       # Save run's final coefficients
       run_coeffs <- coeffs[final_iter, ]
-      
+
       # Record this run's values
       run_vals[run, ] <- c(MSE_run[final_iter], final_iter, run_coeffs)
-      
+
       # Save full MSE and coefficient paths if full = T
       if(full == T){
-         
-         MSE_matrix[1:final_iter, run] <- MSE_run[1:final_iter] 
+
+         MSE_matrix[1:final_iter, run] <- MSE_run[1:final_iter]
          coef_array[1:final_iter, run, ] <- coeffs[1:final_iter, ]
       }
-      
+
    }
-   
+
    ####################################################################################
    # Use the coefficients corresponding to the lowest MSE
-   
+
    final_MSE <- min(run_vals[,1], na.rm = T)
    final_coeffs <- run_vals[which(run_vals[,1] == final_MSE), -(1:2)]
-   
+
    #-----------------------------------------------------------
    # Generate plot if figure = T
    if(figure == T){
-      
+
       # Too many dimensions
       if(ncol(X) > 2){cat("\nFigures only possible when N features < 3\n\n")}
-      
+
       # 3-dimensional plot
       if(ncol(X) == 2){
-         
+
          # Load 'scatterplot3d' package or tell user to install
-         try(library(scatterplot3d), 
+         try(library(scatterplot3d),
              outFile = cat("\nPlease install 'scatterplot3d' library first\n"))
-         
-         # Generate X sequences 
+
+         # Generate X sequences
          x_seq <- seq(0, max(X), length = 1000)
-         
+
          # Plot data and add line
          plt <- scatterplot3d(X[,1], X[,2], y, xlab = "Feature_1", ylab = "Feature_2",
                               zlab = "Output", font.lab = 2, font.axis = 2)
-         
+
          plt$points3d(x_seq, x_seq, gen_preds(final_coeffs, cbind(x_seq, x_seq)),
                       col = "red", type = 'l')
-         
+
       }
-      
+
       # 2-dimensional plot
       if(ncol(X) == 1){
-         
+
          # Generate X sequences
          x_seq <- seq(0, max(X), length = 1000)
-         
+
          plot(X, y, font.lab = 2, font.axis = 2, main = "Linear regression", pch = 19,
               col = "gray40", cex = 1.3, las = 1, cex.lab = 1.2, cex.axis = 1.2)
          points(X, y, cex = 1.3)
-         lines(x_seq, gen_preds(final_coeffs, x_seq), col = "deepskyblue4", lwd = 3, 
+         lines(x_seq, gen_preds(final_coeffs, x_seq), col = "deepskyblue4", lwd = 3,
                lty = 2)
          abline(lm(y ~ X), col = "orange", lty = 2, lwd = 3)
          par(font = 2)
-         legend("top", bty = 'n', c("Gradient descent", "Analytical"), pch = 19, 
+         legend("top", bty = 'n', c("Gradient descent", "Analytical"), pch = 19,
                 col = c("deepskyblue4", "orange"), pt.cex = 1.2)
-         
+
       }
    }
-   
+
    #-----------------------------------------------------------
    # Create list for output
-   
+
    # If X is 1-dimensional, just use lm
    if(ncol(X) == 1){
-      
+
       # Run analytical regression
       reg <- lm(y ~ X)
-      
+
       # Generate data frames of coefficients and MSE
       beta_df <- data.frame("grad_desc" = final_coeffs,
                             "analytical" = coef(reg))
-      
+
       MSE_df <- data.frame("grad_desc" = final_MSE,
                            "analytical" = mean(residuals(reg)^2))
-      
+
    } else {
-      
+
       # Run analytical regression
       lm_reg <- analytical_reg(X, y)
-      
+
       # Generate data frames of coefficients and MSE
       beta_df <- data.frame("grad_desc" = final_coeffs,
                             "analytical" = t(lm_reg$betas))
-      
+
       MSE_df <- data.frame("grad_desc" = final_MSE,
                            "analytical" = lm_reg$MSE)
-      
+
    }
-   
+
    # Add run number to MSE
    run_vals <- cbind("run_number" = 1:n_runs, run_vals)
-   
+
    # Rearrange run_vals data
    ordered_runs <- run_vals[order(run_vals[,2]), ]
-   
+
    #------------------------------------------------------------
    # Create output list
-   
+
    if(full == F){
-      
+
       our_list <- list(ordered_runs, beta_df, MSE_df)
       names(our_list) <- c("run_outcomes", "betas", "final_MSE")
-      
+
       return(our_list)
-      
+
    } else {
-      
+
       our_list <- list(MSE_matrix, coef_array, ordered_runs, beta_df, MSE_df)
       names(our_list) <- c("MSE_paths", "coeff_paths", "run_outcomes", "betas", "final_MSE")
-      
+
       return(our_list)
-      
+
    }
-   
-} 
+
+}
