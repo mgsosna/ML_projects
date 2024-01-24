dc3f3324e74036d7a697effd79f839a006733c7c
mgsosna <matt.g.sosna@gmail.com>
2024-01-23 21:00:31 -0500

    Add high-dimensional data generator

--- random_forest/run.py
+++ random_forest/run.py
@@ -6,7 +6,7 @@ from sklearn.metrics import accuracy_score
 from classes import DecisionTree, Node, RandomForest
 
 # Set seed for reproducibility
-np.random.seed(42)
+np.random.seed(0)
 
 # Data generation helpers. Only parameter to pass in is the mean.
 npn = partial(np.random.normal, scale=1, size=1)
@@ -27,14 +27,28 @@ def gen_df(n: int) -> pd.DataFrame:
 
 # TODO: write a function for high-dimensional data
 # Use this function for a random forest using many features
-#def gen_df_hd(n: int) -> pd.DataFrame:
-#    labels = np.random.choice([0,1], n)
-#
+def gen_df_hd(n_rows: int, n_cols: int) -> pd.DataFrame:
+
+    labels = np.random.choice([0,1], n_rows)
+    d = {'label': labels}
+
+    for i in range(n_cols):
+        mean = np.random.uniform(0, 1, 1)[0]
+        d[f"continuous_{i}"] = [
+            npn(mean)[0] if x else npn(0)[0] for x in labels
+        ]
+        ratio = np.random.uniform(0.4, 0.6, 1)[0]
+        d[f"categorical_{i}"] = [
+            npc(p=[ratio, 1-ratio])[0] if x else npc(p=[0.5,0.5])[0]
+            for x in labels
+        ]
+
+    return pd.DataFrame(d)
 
 # Generate data
 print("Generating train and test data")
-train_df = gen_df(500)
-test_df = gen_df(100)
+train_df = gen_df_hd(400, 50)
+test_df = gen_df_hd(100, 50)
 
 # 1. Decision Tree
 print("1. Fitting a decision tree")
@@ -45,16 +59,20 @@ tree_accuracy = round(accuracy_score(test_df['label'], tree_preds), 3)
 
 # 2. Random Forest
 print("2. Fitting a random forest")
-forest = RandomForest(train_df, target_col='label', n_trees=20)
+forest = RandomForest(train_df, target_col='label', n_trees=50)
 forest.train()
 forest_preds = forest.classify(test_df)
-forest_tree_preds = forest.forest[1].classify(test_df)
-
 forest_accuracy = round(accuracy_score(test_df['label'], forest_preds), 3)
-forest_tree_accuracy = round(accuracy_score(test_df['label'], forest_tree_preds), 3)
+
+# Get accuracy of average tree in forest
+tree_accs = []
+for i in range(forest.n_trees):
+    forest_tree_preds = forest.forest[i].classify(test_df)
+    tree_accs.append(accuracy_score(test_df['label'], forest_tree_preds))
+forest_tree_accuracy = np.mean(tree_accs).round(3)
 
 # Display results
 print("Accuracy")
 print(f" * Single decision tree: {tree_accuracy}")
-print(f" * Tree in random forest: {forest_tree_accuracy}")
+print(f" * Average random forest tree: {forest_tree_accuracy}")
 print(f" * Full random forest: {forest_accuracy}")
